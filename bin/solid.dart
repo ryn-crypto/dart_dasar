6561dc3a7558386df5e52fd235ab79000b771fcd
Riyan First Tiyanto <riyanfirst13@gmail.com>
2022-03-23 21:42:10 +0700

    SOLID Principles

---
+++ bin/solid.dart
@@ -0,0 +1,9 @@
+// solid principles
+void main(List<String> args) {
+  // S-inggle Responsibility Principle
+  // O-pen / close principle
+  // L-iskov Subtitution Principle
+  // I-nterface Segregation Principle
+  // D-ependency Inversion Principle
+  
+}
